eca61ed407d1ff2e5867f9b32977211f7cfb42c9
christopherorea <c28_09_98@hotmail.com>
2023-10-30 18:57:03 -0600

    personal added chrome and removed pngs

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const url = process.argv[2];
 
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await launch({ headless: true });
+  const browser = await launch({ 
+    headless: true, 
+    executablePath: 'C:/Users/c28_0/OneDrive/Documentos/Projects/KG-Neo4j/chrome/win64-118.0.5993.70/chrome-win64/chrome.exe' 
+  });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
@@ -23,9 +26,9 @@ const url = process.argv[2];
   await page.click(options);
 
   // Opens transcript option
-  await page.screenshot({ path: 'check_for_transcript.png' })
+  //await page.screenshot({ path: 'check_for_transcript.png' })
   console.log('Opens transcript option');
-  await page.screenshot({ path: 'transcript_option.png' });
+  //await page.screenshot({ path: 'transcript_option.png' });
   const show = '#items > ytd-menu-service-item-renderer';
   await page.waitForSelector(show, { timeout: 10000 });
   await page.click(show);
@@ -37,7 +40,7 @@ const url = process.argv[2];
     document.querySelector(loadTranscript).click();
     window.scroll(0,0);
   });
-  await page.screenshot({ path: 'click.png' });
+  //await page.screenshot({ path: 'click.png' });
 
   // Reads transcription
   console.log('Reads transcript text');
@@ -49,10 +52,10 @@ const url = process.argv[2];
     const content = Array.from(transcript).map(x => x.innerText);
     return content;
   });
-  await page.screenshot({ path: 'show_transcript.png' });
+  //await page.screenshot({ path: 'show_transcript.png' });
 
   // Create a file
-  writeFile('output.txt', content.toString(), (err) => {
+  writeFile('output.txt', content.toString().replace(/,?[0-9]+:[0-9][0-9]/g, "").replace(/^\s*\n/gm, ""), (err) => {
     if (err) throw err;
   });
 
